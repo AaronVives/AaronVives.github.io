888f80f058657bbf6d2f2af26f5095acb55e2881
AaronVives <aaron.vandaele@student.vives.be>
2024-03-09 11:22:39 +0100

    an

--- Sem2/Labo4/OpdrachtManvanAn/scripts/index.js
+++ Sem2/Labo4/OpdrachtManvanAn/scripts/index.js
@@ -1,8 +1,15 @@
-const setup = () => {
-    const belangrijkeParagrafen = document.getElementsByClassName('belangrijk');
-    for (let i = 0; i < belangrijkeParagrafen.length; i++) {
-        belangrijkeParagrafen[i].classList.add('opvallend');
-    }
+const Setup = () => {
+    const btn = document.getElementById("btn");
+    btn.addEventListener("click", searchForAn);
 }
 
-window.addEventListener("load", setup);
+const searchForAn = () =>{
+    const text = "De man van An geeft geen hand aan ambetante verwanten.".toLowerCase();
+    console.log(text);
+    let count = -1;
+    for(let last = 0; last !== -1; count++){
+        last = text.indexOf("an", last + 1);
+    }
+    console.log(count + " keer an in tekst.");
+}
+window.addEventListener("load", Setup);
