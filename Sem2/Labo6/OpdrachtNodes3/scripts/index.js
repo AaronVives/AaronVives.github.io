8030dce4726b3b8fa2a748cddcaa7294a983fcf4
AaronVives <146540147+AaronVives@users.noreply.github.com>
2024-03-22 20:55:18 +0100

    p3

--- Sem2/Labo6/OpdrachtNodes3/scripts/index.js
+++ Sem2/Labo6/OpdrachtNodes3/scripts/index.js
@@ -1,22 +1,11 @@
+const appendParagraph = () => {
+    const divElement = document.getElementById("myDIV");
+    const paragraphElement = document.createElement("p");
+    paragraphElement.textContent = "some text";
+    divElement.appendChild(paragraphElement);
+}
 const setup = () => {
-    document.getElementById("submit").addEventListener("click", output);
+    const inputButton = document.getElementById("input_button");
+    inputButton.addEventListener("click", appendParagraph);
 }
-
-const output = () => {
-    console.clear();
-
-    console.log(document.getElementById("is_roker").checked ? "is een roker" : "is geen roker");
-
-    const taalElement = document.querySelector("input[name='taal']:checked");
-    console.log(`moedertaal is ${taalElement ? taalElement.value : "niet geselecteerd"}`);
-
-    console.log(`favoriete buurland is ${document.getElementById("favorite_country").value}`);
-
-    const bestellingOptions = document.getElementById("order").selectedOptions;
-    const bestellingArray = Array.from(bestellingOptions, option => option.value);
-    console.log(bestellingArray.length > 0 ? `bestelling bestaat uit ${bestellingArray.join(" ")}` : "geen bestelling geselecteerd");
-
-    console.log("-".repeat(40));
-}
-
 window.addEventListener("load", setup);
